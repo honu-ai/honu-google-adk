f45815af45738d41d8de3bab98435d00d5d61cb8
gonzalogacc <gonzalogacc@gmail.com>
2025-08-15 13:14:09 +0200

    Move thing from utils.

--- honu_google_adk/main.py
+++ honu_google_adk/main.py
@@ -1,38 +1,42 @@
 import json
 from typing import Optional
 
+from fastmcp import Client
+from fastmcp.client import StreamableHttpTransport
 from google.adk.agents.readonly_context import ReadonlyContext
-from google.adk.tools import BaseTool, ToolContext, FunctionTool
+from google.adk.tools import BaseTool, FunctionTool
+from google.adk.tools import ToolContext
 from google.adk.tools.base_toolset import BaseToolset
 
-from honu_google_adk.utils import _get_client, _get_unauth_client
-
-def create_tool(function_name: str, function_description: str):
-
-    async def _inner(args: dict, tool_context: ToolContext):
-        client = _get_client(tool_context)
-
-        async with client:
-            # Execute operations
-            result = await client.call_tool(function_name, args)
-
-        return dict(
-            message='success',
-            artefacts=json.loads(result.content[0].text)
-        )
-
-    _inner.__doc__ = function_description
-    _inner.__name__= function_name
-
-    return _inner
 
 class HonuToolSet(BaseToolset):
     tags: set[str] | None = None
 
-    def __init__(self, *tags_to_filter_by: str):
+    def __init__(self, mcp_host: str, *tags_to_filter_by: str):
+        self.mcp_host = mcp_host
         if len(tags_to_filter_by):
             self.tags = set(tags_to_filter_by)
         super().__init__(tool_filter=None)
+
+    def _get_unauth_client(self):
+        client = Client(
+            transport=StreamableHttpTransport(self.mcp_host),
+        )
+        return client
+
+    def _get_client(self, tool_context: ToolContext):
+        token = tool_context.state.get('token')
+        model_ref = tool_context.state.get('model_ref')
+
+        # In-memory server (ideal for testing)
+        client = Client(
+            transport=StreamableHttpTransport(
+                self.mcp_host,
+                headers={"X-HONU-MODEL": model_ref},
+            ),
+            auth=token,
+        )
+        return client
 
     @staticmethod
     def _is_valid_tool(valid_tool_tags: set[str], tool: BaseTool) -> bool:
@@ -52,14 +56,36 @@ class HonuToolSet(BaseToolset):
             self,
             readonly_context: Optional[ReadonlyContext] = None,
     ) -> list[BaseTool]:
-        client = _get_unauth_client()
+        client = self._get_unauth_client()
         tools=[]
         async with client:
             for tool in await client.list_tools():
                 if self._is_valid_tool(self.tags, tool):
-                    tools.append(FunctionTool(create_tool(tool.name,tool.description)))
+                    tools.append(
+                        FunctionTool(
+                            self.create_tool(tool.name,tool.description)
+                        )
+                    )
 
         return tools
+
+    def create_tool(self, function_name: str, function_description: str):
+        async def _inner(args: dict, tool_context: ToolContext):
+            client = self._get_client(tool_context)
+
+            async with client:
+                # Execute operations
+                result = await client.call_tool(function_name, args)
+
+            return dict(
+                message='success',
+                artefacts=json.loads(result.content[0].text)
+            )
+
+        _inner.__doc__ = function_description
+        _inner.__name__ = function_name
+
+        return _inner
 
     async def close(self):
         return None
