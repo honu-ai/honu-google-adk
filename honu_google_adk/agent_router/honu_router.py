0785aaa8744faca83c40d339c4f9f019a74b86e1
freyamade <frybrdrck@gmail.com>
2025-09-08 17:55:37 +0200

    add instruction to the agent to introduce themselves on engage

--- honu_google_adk/agent_router/honu_router.py
+++ honu_google_adk/agent_router/honu_router.py
@@ -1,6 +1,7 @@
 import base64
 
 import json
+from datetime import datetime, timezone
 from fastapi import APIRouter
 from google.adk.cli.adk_web_server import RunAgentRequest
 from google.adk.events import Event
@@ -10,7 +11,7 @@ from starlette import status
 from starlette.exceptions import HTTPException
 
 from .conversation_utils import ConversationClient
-from .schema import InitEngagement, DisengageAgent, MessageNotification, TextMessage, AgentDisplayInformation
+from .schema import HAPMessage, InitEngagement, DisengageAgent, MessageNotification, TextMessage, AgentDisplayInformation
 from .utils import LocalSessionClient
 
 
@@ -37,52 +38,6 @@ class HonuAgentRouter:
 
     def _agent_engagement_api(self) -> APIRouter:
         api = APIRouter(prefix="/hapra/v1", tags=['adk'])
-
-        @api.get("/health_check/ping/{value}", status_code=status.HTTP_200_OK)
-        def ping_pong(value: str) -> str:
-            return value
-
-        @api.get('/cards/{app_name}/', include_in_schema=False)
-        @api.get('/cards/{app_name}')
-        def get_agent_card(app_name: str) -> AgentDisplayInformation:
-            card = self.display_info.get(app_name)
-            if card is None:
-                card = AgentDisplayInformation(
-                    name=app_name,
-                    avatar_url=None,
-                    description='An Agent built to help you!',
-                )
-            return card
-
-        @api.post("/agents/{agent_id}/init_engagement/", status_code=status.HTTP_201_CREATED, include_in_schema=False)
-        @api.post("/agents/{agent_id}/init_engagement", status_code=status.HTTP_201_CREATED)
-        def init_engagement(agent_id: str, init: InitEngagement) -> None:
-            conv_client = ConversationClient.get_instance()
-            # Get the data from the agent signature for making the chat name
-            sig_payload = SignaturePayload.from_signature(init.agent_signature)
-            conv = conv_client.create_conversation(init.auth_token, init.mdl_ref, name=sig_payload.app_name)
-            session_id = conv.conversation_id
-
-            payload = dict(
-                token=init.auth_token,
-                model_ref=init.mdl_ref
-            )
-
-            response = self.local_session_client.client.post(f"/apps/{agent_id}/users/{self.USER_ID}/sessions/{session_id}", json=payload)
-            if response.status_code != status.HTTP_200_OK:
-                raise HTTPException(status_code=response.status_code, detail=response.text)
-
-        @api.post("/agents/{agent_id}/disengage/", status_code=status.HTTP_200_OK, include_in_schema=False)
-        @api.post("/agents/{agent_id}/disengage", status_code=status.HTTP_200_OK)
-        def disengage_agent(agent_id: str, disengage: DisengageAgent) -> None:
-            conversation_client = ConversationClient.get_instance()
-            sessions = self.local_session_client.get_sessions_for_model_ref(agent_id, disengage.mdl_ref)
-            for token, conv_id in sessions:
-                try:
-                    conversation_client.delete_conversation(token, disengage.mdl_ref, conv_id)
-                except:
-                    pass
-                self.local_session_client.delete_session(agent_id, conv_id)
 
         @api.post("/messages/", status_code=status.HTTP_200_OK, include_in_schema=False)
         @api.post("/messages", status_code=status.HTTP_200_OK)
@@ -136,5 +91,64 @@ class HonuAgentRouter:
                         print('Unhandled message type')
                         print(message)
             conversation_client.set_chat_status(token, conv, None)
+
+        @api.get("/health_check/ping/{value}", status_code=status.HTTP_200_OK)
+        def ping_pong(value: str) -> str:
+            return value
+
+        @api.get('/cards/{app_name}/', include_in_schema=False)
+        @api.get('/cards/{app_name}')
+        def get_agent_card(app_name: str) -> AgentDisplayInformation:
+            card = self.display_info.get(app_name)
+            if card is None:
+                card = AgentDisplayInformation(
+                    name=app_name,
+                    avatar_url=None,
+                    description='An Agent built to help you!',
+                )
+            return card
+
+        @api.post("/agents/{agent_id}/init_engagement/", status_code=status.HTTP_201_CREATED, include_in_schema=False)
+        @api.post("/agents/{agent_id}/init_engagement", status_code=status.HTTP_201_CREATED)
+        def init_engagement(agent_id: str, init: InitEngagement) -> None:
+            conv_client = ConversationClient.get_instance()
+            # Get the data from the agent signature for making the chat name
+            sig_payload = SignaturePayload.from_signature(init.agent_signature)
+            conv = conv_client.create_conversation(init.auth_token, init.mdl_ref, name=sig_payload.app_name)
+            session_id = conv.conversation_id
+
+            payload = dict(
+                token=init.auth_token,
+                model_ref=init.mdl_ref
+            )
+
+            response = self.local_session_client.client.post(f"/apps/{agent_id}/users/{self.USER_ID}/sessions/{session_id}", json=payload)
+            if response.status_code != status.HTTP_200_OK:
+                raise HTTPException(status_code=response.status_code, detail=response.text)
+
+            # Send a fake message to the agent to prompt an introduction message to the user
+            fake_message = MessageNotification(
+                agent_signature=init.agent_signature,
+                conversation=conv,
+                message=HAPMessage(
+                    message_id='',
+                    author_id='',
+                    timestamp=datetime.now(timezone.utc),
+                    payload=TextMessage(body='You have just been engaged by a User. Please introduce yourself to them.'),
+                )
+            )
+            message_notification(fake_message)
+
+        @api.post("/agents/{agent_id}/disengage/", status_code=status.HTTP_200_OK, include_in_schema=False)
+        @api.post("/agents/{agent_id}/disengage", status_code=status.HTTP_200_OK)
+        def disengage_agent(agent_id: str, disengage: DisengageAgent) -> None:
+            conversation_client = ConversationClient.get_instance()
+            sessions = self.local_session_client.get_sessions_for_model_ref(agent_id, disengage.mdl_ref)
+            for token, conv_id in sessions:
+                try:
+                    conversation_client.delete_conversation(token, disengage.mdl_ref, conv_id)
+                except:
+                    pass
+                self.local_session_client.delete_session(agent_id, conv_id)
 
         return api
